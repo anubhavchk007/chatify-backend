d5e961f09c4b224948f36ef6e744b917f5e3b8a1
anubhavchk007 <anubhavclasher2262@gmail.com>
2025-06-21 18:08:58 +0530

    fixed a lot of things

--- src/main/java/com/chatify/chatify_backend/config/AppConstants.java
+++ src/main/java/com/chatify/chatify_backend/config/AppConstants.java
@@ -1,6 +1,6 @@
 package com.chatify.chatify_backend.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "https://chatify-frontend-taupe.vercel.app/";
+    public static final String FRONT_END_BASE_URL = "https://chatify-frontend-taupe.vercel.app";
 
 }
